88e3c70f0c2750d41295967431d45d5741180c00
Arthur Marichal <arthur.marichal@le-campus-numerique.fr>
2022-01-27 15:25:12 +0100

    Test unitaire: fizzBuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,3 +1,15 @@
 export function fizzBuzz(n) {
-  // A completer
+    var three = n % 3;
+    var five = n % 5;
+    if (three === 0 && five === 0){
+        return 'FizzBuzz'
+    }
+    if (three === 0){
+    return 'fizz'
+    }
+    if (five === 0){
+        return 'buzz'
+    }
+    throw new Error('Not multiples of 3 or 5 !')
+
 }
--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -2,7 +2,8 @@ import {fizzBuzz} from "../src/fizzbuzz";
 
 test('should return fizz on multiples of 3', () => {
   expect(fizzBuzz(3)).toBe('fizz');
-  expect(fizzBuzz(6)).toBe('fizz');
+  expect(fizzBuzz(5)).toBe('buzz');
+  expect(fizzBuzz(15)).toBe('FizzBuzz');
 });
 
 // A completer
