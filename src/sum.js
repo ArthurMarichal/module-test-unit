6933439e075a5eda0e558cef63fd702661ae8740
Arthur Marichal <arthur.marichal@le-campus-numerique.fr>
2022-01-27 14:12:38 +0100

    Test unitaire: addition

--- src/sum.js
+++ src/sum.js
@@ -1,3 +1,7 @@
-// Implement the sum function here
 export function sum(a, b) {
+    if (typeof a === 'number'&& typeof b === 'number') {
+    return a + b;
+    } else {
+        throw new Error('Une des valeurs n\'est pas un nombre !');
+    }
 }
