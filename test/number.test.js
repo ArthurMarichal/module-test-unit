76e296c5ae59006ddb81096a5f306b29bcec3d09
Arthur Marichal <arthur.marichal@le-campus-numerique.fr>
2022-01-28 09:41:38 +0100

    Test unitaire: refactor sum.test

--- test/number.test.js
+++
@@ -1,5 +0,0 @@
-import {sum} from "../src/sum";
-
-test("Should throw an error if it's not numbers", () => {
-    expect( () => {sum('a','b')}).toThrow('Une des valeurs n\'est pas un nombre !')
-})
--- test/sum.test.js
+++ test/sum.test.js
@@ -4,3 +4,7 @@ import {sum} from "../src/sum";
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1,2)).toBe(3);
 });
+
+test("Should throw an error if it's not numbers", () => {
+    expect( () => {sum('a','b')}).toThrow('Une des valeurs n\'est pas un nombre !')
+})
